3744d29ae9d08b58ba955b1acddf48c7f2a8d920
irsyaad06 <irsyadfatahilah@gmail.com>
2025-11-11 15:27:00 +0700

    only admin can access filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
+use Illuminate\Support\Facades\Log;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -25,13 +28,23 @@ class User extends Authenticatable
         return $this->hasOne(Vote::class);
     }
 
-    protected $hidden = [
-    ];
+    protected $hidden = [];
 
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
         ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        Log::info('canAccessPanel dipanggil untuk user id: ' . $this->id);
+        return true;
+    }
+    public function canAccessFilament(): bool
+    {
+        Log::info('canAccessFilament dipanggil untuk admin id: ' . $this->id);
+        return true;
     }
 }
